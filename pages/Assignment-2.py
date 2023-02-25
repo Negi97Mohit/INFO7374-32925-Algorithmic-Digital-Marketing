b8c96f0adefb2fd861a8ce5eb48193ad98d6cd7c
Mohit Negi <62002911+Negi97Mohit@users.noreply.github.com>
2023-02-24 20:58:59 -0500

    Update Assignment-2.py

--- pages/Assignment-2.py
+++ pages/Assignment-2.py
@@ -9,18 +9,17 @@ import altair as alt
 import pandas as pd
 from snowflake.snowpark.session import Session
 from snowflake.snowpark.functions import col
+import plotly.express as px
 import streamlit as st
 import datetime as dt
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-APP_ICON_URL = "https://i.imgur.com/dBDOHH3.png"
-
 # Function to create Snowflake Session to connect to Snowflake
 def create_session():
     if "snowpark_session" not in st.session_state:
-        session = Session.builder.configs(json.load(open("connection.json"))).create()
+        session = Session.builder.configs(json.load(open("pages\connection.json"))).create()
         st.session_state['snowpark_session'] = session
     else:
         session = st.session_state['snowpark_session']
@@ -41,14 +40,19 @@ def get_date_int(df, column) :
     year = df[column].dt.year
     month = df[column].dt.month
     day = df[column].dt.day
-    
     return year, month, day
 
-
 def cohortAnalysis(df):
-    df.rename(columns={'CUSTOMER_ID':'customer_id','TRANSACTION_DATE':'transaction_date','ONLINE_ORDER':'online_order','ORDER_STATUS':'order_status'},inplace=True)  
+    df.columns=['transaction_id', 'product_id', 'customer_id', 'transaction_date',
+       'online_order', 'order_status', 'brand', 'product_line',
+       'product_class', 'product_size', 'list_price', 'standard_cost',
+       'product_first_sold_date']
+    df.transaction_date  = pd.to_datetime(df['transaction_date'])
+    df.order_status=df['order_status'].astype('str')
+    # df.rename(columns={'CUSTOMER_ID':'customer_id','TRANSACTION_DATE':'transaction_date','ONLINE_ORDER':'online_order','ORDER_STATUS':'order_status'},inplace=True)
+    df.transaction_date  = pd.to_datetime(df['transaction_date'])
     df_final = df[['customer_id','transaction_date','online_order','order_status']]
-    df_final = df_final[df_final['order_status'] == 'Approved']
+    df_final = df_final[df_final['order_status'] == 'True']
     df_final = df_final[~df_final.duplicated()]
     df_final['transaction_date'] = pd.to_datetime(df['transaction_date'])
     df_final['transaction_month'] = df['transaction_date'].apply(get_month)
@@ -57,9 +61,10 @@ def cohortAnalysis(df):
     group = df_final.groupby('customer_id')['transaction_month']
     df_final['cohort_month'] = group.transform('min')
 
-
+    # st.write(df_final)
     transaction_year, transaction_month, transaction_day = get_date_int(df_final, 'transaction_month')
     cohort_year, cohort_month, cohort_day = get_date_int(df_final,'cohort_month')
+    
     #Calculate Year Differences
     years_diff = transaction_year - cohort_year
 
@@ -67,6 +72,7 @@ def cohortAnalysis(df):
     months_diff = transaction_month - cohort_month
 
     df_final['cohort_index'] = years_diff*12 + months_diff + 1
+
     #Final Grouping to Calculate Total Unique Users in Each Cohort
     cohort_group = df_final.groupby(['cohort_month','cohort_index'])
 
@@ -77,8 +83,8 @@ def cohortAnalysis(df):
                                             columns = 'cohort_index',
                                             values = 'customer_id'   
                                         )
+    
     #Calculate Retention rate per Month Index
-
     cohort_size = cohort_counts.iloc[:,0]
 
     retention = cohort_counts.divide(cohort_size, axis = 0)
@@ -86,23 +92,13 @@ def cohortAnalysis(df):
     retention = retention.round(3)*100
 
     retention.index = retention.index.strftime('%Y-%m')
-    plt.figure(figsize = (16,10))
 
-    plt.title('MoM Retention Rate for Customer Transaction Data')
-
-    sns.heatmap(retention, annot = True, cmap="YlGnBu", fmt='g')
-
-    plt.xlabel('Cohort Index')
-    plt.ylabel('Cohort Month')
-    plt.yticks(rotation = '360')
-
-    plt.show()
-
+    fig = px.imshow(retention, text_auto=True)
+    st.plotly_chart(fig)
 
 # Streamlit config
-st.set_page_config("Assignment-2: Cohort Analysis", APP_ICON_URL, "centered")
+st.set_page_config("Assignment-2: Cohort Analysis", "centered")
 st.write("<style>[data-testid='stMetricLabel'] {min-height: 0.5rem !important}</style>", unsafe_allow_html=True)
-st.image(APP_ICON_URL, width=80)
 st.title("Assignment-2: Cohort Analysis")
 
 # Call functions to get Snowflake session and load data
@@ -110,4 +106,4 @@ session = create_session()
 transactions=load_data()
 
 st.write(transactions)
-# cohortAnalysis(transactions)
+cohortAnalysis(transactions)
